ed2e07d057c8ba8cbc76eb85a29140e029d21dea
trofim110 <trofim.chuchuk@mail.ru>
2022-06-28 12:11:29 +0700

    Done

--- A function that checks character/Enter_two_numbers.cs
+++ A function that checks character/Enter_two_numbers.cs
@@ -8,19 +8,41 @@ namespace Enter_two_numbers
 {
     public class  Validationofvalues
     {
-        public  static int Compares_characters(ref string Value_1,ref string Value_2)
+        public  int Compares_characters( int a, int b)
         {
-            return String.Compare( Value_1, Value_2);
+            int l=0;
+            if ((a>47)&&(a<58)&&(b>47)&&(b<58))   //Если оба из символов не являются буквенными, то вернуть -1
+            {
+                l--;
+            }
+            else if ((a>64&&b>64&&a<91&&b<91)||(a>96&&b>96&&a<123&&b<123))
+            {
+                l++;   //Если оба символа являются буквенными, и находятся в одном регистре, то вернуть 1
+
+            }
+            else if ((a > 64 && b > 96 && a < 91 && b < 123) || (a > 96 && b > 64 && a < 123 && b < 91))
+            {
+                //Если оба символа являются буквенными, но находятся в разных регистрах, то вернуть 0
+
+            }
+
+            return l;
         }
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Введите два символа через entr (либо букву либо цифру) ");
             string Value_1, Value_2;
+            var c = new Validationofvalues();
             Value_1 = Console.ReadLine();
             Value_2 = Console.ReadLine();
+
+
             if (Value_1.Length < 2 || Value_2.Length < 2)  //Проверка ,что пользователь ввел по одному сиволу
             {
-                Console.WriteLine(Compares_characters(ref Value_1,ref Value_2));
+                int a, b,result;
+                a=  Value_1[0];
+                b=  Value_1[0];
+                Console.WriteLine(c.Compares_characters( a, b));
             }
             else
             {
--- Enter_two_numbersTest/Enter_two_numbersTest.cs
+++ Enter_two_numbersTest/Enter_two_numbersTest.cs
@@ -1,20 +1,59 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Enter_two_numbers;
+
 namespace Enter_two_numbersTest;
 
 [TestClass]
 public class UnitTest1
 {
     [TestMethod]
-    public void  minusOne()
+    public void  minusOne() //Если оба из символов не являются буквенными, то вернуть -1
     {
         //arrnge
         string  Value_1="1";
         string Value_2 = "2";
         int result=(-1);
+        int a, b;
+        a=  Value_1[0];
+        b=  Value_1[0];
         //act
         Validationofvalues c = new Validationofvalues();
-        int actual = c.Compares_characters(ref  Value_1,ref Value_2);
+
+        int actual = c.Compares_characters(a,b);
+        //assert
+        Assert.AreEqual(result,actual);
+    }
+    [TestMethod]
+    public void  Zero()  //Если оба символа являются буквенными, но находятся в разных регистрах, то вернуть 0
+    {
+        //arrnge
+        string  Value_1="R";
+        string Value_2 = "i";
+        int result=(1);
+
+        int a, b;
+        a=  Value_1[0];
+        b=  Value_1[0];
+
+        //act
+        Validationofvalues c = new Validationofvalues();
+        int actual = c.Compares_characters( a,b);
+        //assert
+        Assert.AreEqual(result,actual);
+    }
+    [TestMethod]
+    public void  one()        //Если оба символа являются буквенными, и находятся в одном регистре, то вернуть 1
+    {
+        //arrnge
+        string  Value_1="e";
+        string Value_2 = "r";
+        int result=(1);
+        int a, b;
+        a=  Value_1[0];
+        b=  Value_1[0];
+        //act
+        Validationofvalues c = new Validationofvalues();
+        int actual = c.Compares_characters(a,b);
         //assert
         Assert.AreEqual(result,actual);
     }
